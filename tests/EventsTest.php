ba64f152df5f9a384af28827611192253ff7a217
Natan Felles <natanfelles@gmail.com>
2021-06-12 16:17:42 -0300

    Update Coding Standard

--- tests/EventsTest.php
+++ tests/EventsTest.php
@@ -3,21 +3,21 @@
 use Framework\Events\Events;
 use PHPUnit\Framework\TestCase;
 
-class EventsTest extends TestCase
+final class EventsTest extends TestCase
 {
-	public function testEvent()
+	public function testEvent() : void
 	{
-		$this->assertFalse(Events::isListening('foo'));
-		Events::listen('foo', static function ($a) {
+		self::assertFalse(Events::isListening('foo'));
+		Events::listen('foo', static function ($a) : void {
 			echo 'bar ' . $a;
 		});
-		$this->assertTrue(Events::isListening('foo'));
+		self::assertTrue(Events::isListening('foo'));
 		\ob_start();
 		Events::trigger('foo', 1);
 		$contents = \ob_get_clean();
-		$this->assertEquals('bar 1', $contents);
+		self::assertSame('bar 1', $contents);
 		Events::remove('foo');
-		$this->assertFalse(Events::isListening('foo'));
+		self::assertFalse(Events::isListening('foo'));
 		$this->expectException(\OutOfBoundsException::class);
 		$this->expectExceptionMessage('Undefined event with name "foo"');
 		Events::trigger('foo');
