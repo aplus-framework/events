f3760b3210724eb16bb5bbe29ec61440a3945258
Natan Felles <natanfelles@gmail.com>
2021-06-27 16:31:54 -0300

    Coding Standard: Update version and add header comments

--- tests/EventsTest.php
+++ tests/EventsTest.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\Events;
+<?php
+/*
+ * This file is part of The Framework Events Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Events;
 
 use Framework\Events\Events;
 use PHPUnit\Framework\TestCase;
