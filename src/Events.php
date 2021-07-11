b519e7fb324f60da2250bd7deaa436c481661a4b
Natan Felles <natanfelles@gmail.com>
2021-07-11 15:12:16 -0300

    Indent files with space

--- src/Events.php
+++ src/Events.php
@@ -17,32 +17,32 @@ use OutOfBoundsException;
  */
 class Events
 {
-	/**
-	 * @var array<string,callable>
-	 */
-	protected static array $listeners = [];
+    /**
+     * @var array<string,callable>
+     */
+    protected static array $listeners = [];
 
-	public static function listen(string $name, callable $callback) : void
-	{
-		static::$listeners[$name] = $callback;
-	}
+    public static function listen(string $name, callable $callback) : void
+    {
+        static::$listeners[$name] = $callback;
+    }
 
-	public static function trigger(string $name, mixed ...$arguments) : void
-	{
-		if (empty(static::$listeners[$name])) {
-			throw new OutOfBoundsException('Undefined event with name "' . $name . '"');
-		}
-		static::$listeners[$name](...$arguments);
-	}
+    public static function trigger(string $name, mixed ...$arguments) : void
+    {
+        if (empty(static::$listeners[$name])) {
+            throw new OutOfBoundsException('Undefined event with name "' . $name . '"');
+        }
+        static::$listeners[$name](...$arguments);
+    }
 
-	public static function remove(string $name) : void
-	{
-		unset(static::$listeners[$name]);
-	}
+    public static function remove(string $name) : void
+    {
+        unset(static::$listeners[$name]);
+    }
 
-	#[Pure]
-	public static function isListening(string $name) : bool
-	{
-		return isset(static::$listeners[$name]);
-	}
+    #[Pure]
+    public static function isListening(string $name) : bool
+    {
+        return isset(static::$listeners[$name]);
+    }
 }
--- tests/EventsTest.php
+++ tests/EventsTest.php
@@ -14,21 +14,21 @@ use PHPUnit\Framework\TestCase;
 
 final class EventsTest extends TestCase
 {
-	public function testEvent() : void
-	{
-		self::assertFalse(Events::isListening('foo'));
-		Events::listen('foo', static function ($a) : void {
-			echo 'bar ' . $a;
-		});
-		self::assertTrue(Events::isListening('foo'));
-		\ob_start();
-		Events::trigger('foo', 1);
-		$contents = \ob_get_clean();
-		self::assertSame('bar 1', $contents);
-		Events::remove('foo');
-		self::assertFalse(Events::isListening('foo'));
-		$this->expectException(\OutOfBoundsException::class);
-		$this->expectExceptionMessage('Undefined event with name "foo"');
-		Events::trigger('foo');
-	}
+    public function testEvent() : void
+    {
+        self::assertFalse(Events::isListening('foo'));
+        Events::listen('foo', static function ($a) : void {
+            echo 'bar ' . $a;
+        });
+        self::assertTrue(Events::isListening('foo'));
+        \ob_start();
+        Events::trigger('foo', 1);
+        $contents = \ob_get_clean();
+        self::assertSame('bar 1', $contents);
+        Events::remove('foo');
+        self::assertFalse(Events::isListening('foo'));
+        $this->expectException(\OutOfBoundsException::class);
+        $this->expectExceptionMessage('Undefined event with name "foo"');
+        Events::trigger('foo');
+    }
 }
